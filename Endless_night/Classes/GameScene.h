e215a6ea6e6266ba11729d8430ece3f2a3d022b9
conorH22 <lackamoredude@gmail.com>
2016-02-28 20:24:57 +0000

    game over collision
    uploading what i have done endgame physics  and collision dection

--- Endless_night/Classes/GameScene.h
+++ Endless_night/Classes/GameScene.h
@@ -1,19 +1,23 @@
-<<<<<<< HEAD
 #ifndef __GAME_SCENE_H__ // used so that hte header will only be used once per compile
 #define __GAME_SCENE_H__
 
 #include "cocos2d.h"// brining in cocos funcality 
 #include "Box2D/Box2D.h"
+#include"chipmunk.h"
 using namespace cocos2d; // namespace for cococs so you dont have to use cocos2d:: every time 
 
 class GameScene : public cocos2d::Layer
 {
 
 private:
-	CCTMXTiledMap*_level1Map;
-	CCTMXLayer *_background;
+	cocos2d::TMXTiledMap *_tileMap;
+	cocos2d::TMXLayer *_background;
+	cocos2d::TMXLayer *_background2;
+	cocos2d::TMXLayer *_background3;
+	cpSpace*m_pSpace;
 	Sprite* _player;
 	Sprite* _player2;
+	Sprite* gameOverBox;
 	unsigned int score;
 	cocos2d::Label * scoreLabel;
 	cocos2d::Label * livesLabel;
@@ -50,67 +54,10 @@ public:
 	void GoToMainMenuScene(Ref *sender);
 	void GoToGameOverScene();
 	CC_SYNTHESIZE_RETAIN(Array*, _waypoints, Waypoints);
+	void chipPhysics();
 	
 	CREATE_FUNC(GameScene);
 	
 };
 
-=======
-#ifndef __GAME_SCENE_H__ // used so that hte header will only be used once per compile
-#define __GAME_SCENE_H__
-
-#include "cocos2d.h"// brining in cocos funcality 
-#include "Box2D/Box2D.h"
-using namespace cocos2d; // namespace for cococs so you dont have to use cocos2d:: every time 
-
-class GameScene : public cocos2d::Layer
-{
-
-private:
-	Sprite* _player;
-	Sprite* _player2;
-	unsigned int score;
-	unsigned int lives;
-	cocos2d::Label * scoreLabel;
-	cocos2d::Label * livesLabel;
-	int enemyDamage;
-	int towerDamage;
-	float towerfireRate;
-	int maxHp;
-	int currentHp;
-	float walkingSpeed;
-	int towerHp;
-	bool gameEnded;
-	bool gameOver;
-	int enemyScore;
-
-public:
-	static cocos2d::Scene* createScene();// creating the scene from here 
-	//GameScene();
-	~GameScene();
-	virtual bool init();// creating the bool init calss 
-	void addMonster(float dt);// creating the monster class
-	bool onTouchBegan(Touch * touch, Event *unused_event);
-	bool onContactBegan(PhysicsContact &contact);
-	void menuCloseCallback(cocos2d::Ref* pSender);
-	bool scored;
-	bool livesLeft;
-	void SetIsScored();
-	bool GetIsScored();
-	void SetLives();
-	bool GetLives();
-	
-
-	bool onContactBeganEndGame(PhysicsContact &contact);
-	void getHpDamage();
-	void doGameOver();
-	void GoToMainMenuScene(Ref *sender);
-	void GoToGameOverScene();
-	CC_SYNTHESIZE_RETAIN(Array*, _waypoints, Waypoints);
-	void addWayPoints();
-	CREATE_FUNC(GameScene);
-
-};
-
->>>>>>> origin/master
 #endif // __GAME_SCENE_H__
